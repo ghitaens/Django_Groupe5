7ddedfb4a899ed401bdb293248efc4afcfc165ea
ghitaens <ghitaenss02@gmail.com>
2023-01-14 21:08:09 +0100

    fourth commit

--- app1/forms.py
+++ app1/forms.py
@@ -3,7 +3,7 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit, Layout, Field
 
 
-
+#Classe de formulaire d'ajout de salle
 class InfosSalleForm(forms.Form):
     name = forms.CharField(max_length=255)
     guests_number = forms.IntegerField()
@@ -11,7 +11,9 @@ class InfosSalleForm(forms.Form):
     whiteboard = forms.BooleanField(required=False)
     the_wifi = forms.BooleanField(required=False)
     projector = forms.BooleanField(required=False)
+    # Méthode pour configurer l'affichage du formulaire en utilisant crispy
     def __init__(self, *args, **kwargs):
+        # Créer un objet helper pour configurer l'affichage du formulaire
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_method = 'post'
@@ -25,7 +27,8 @@ class InfosSalleForm(forms.Form):
             Field('projector'),
         )
     
-    
+
+#Classe de formulaire de recherche de salle   
 class SearchForm(forms.Form):
     invite_nombre = forms.CharField(max_length=100)
     purpose = forms.CharField(max_length=200)
@@ -34,8 +37,10 @@ class SearchForm(forms.Form):
     le_tableau_blanc = forms.BooleanField(required=False)
     la_wifi = forms.BooleanField(required=False)
     le_projecteur = forms.BooleanField(required=False)
+    # Méthode pour configurer l'affichage du formulaire en utilisant crispy
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        # Créer un objet helper pour configurer l'affichage du formulaire
         self.helper = FormHelper()
         self.helper.form_method = 'post'
         self.helper.add_input(Submit('submit', 'Search'))
--- app1/views.py
+++ app1/views.py
@@ -11,21 +11,28 @@ def homeemploye_view(request):
     return render(request, 'home_employe.html')
 
 def insert_view(request):
+    
+    """
+    Gère la soumission de formulaire pour la création de nouvelles salles et
+    enregistre les données dans le modèle InfosSalle
+    """
     if request.method == 'POST':
         form = InfosSalleForm(request.POST)
         if form.is_valid():
-
+        # Créer une instance du modèle InfosSalle avec les données du formulaire
             model_instance = InfosSalle(
-                nom=form.cleaned_data['name'],
-                nombre_invite=form.cleaned_data['guests_number'],
-                machine_a_cafe=form.cleaned_data['cafe_machine'],
-                tableau_blanc=form.cleaned_data['whiteboard'],
-                wifi=form.cleaned_data['the_wifi'],
-                projecteur=form.cleaned_data['projector']
+            nom=form.cleaned_data['name'],
+            nombre_invite=form.cleaned_data['guests_number'],
+            machine_a_cafe=form.cleaned_data['cafe_machine'],
+            tableau_blanc=form.cleaned_data['whiteboard'],
+            wifi=form.cleaned_data['the_wifi'],
+            projecteur=form.cleaned_data['projector']
             )
+        # Enregistrer l'instance dans la base de données
             model_instance.save()
-            
+        # Réinitialiser le formulaire    
             form = InfosSalleForm()
+            
             messages.success(request, 'Salle enregistrée avec succès')
         else :
             messages.error(request, 'Erreur lors de l\'enregistrement de la salle')
@@ -35,29 +42,44 @@ def insert_view(request):
     return render(request, 'ajouter_salle.html', {'form': form})
 
 def view_salle(request):
+    #Affiche toutes les salles enregistrées dans la base de données
     salle = InfosSalle.objects.all()
     return render(request, 'liste_salle.html', {'salle': salle})
 
 def view_reservation(request):
+    #Affiche toutes les réservations de salles enregistrées dans la base de données
     reservation=SalleReserve.objects.all()
     return render(request,'salle_reserve.html',{'reservation':reservation})
 
 def search_view(request):
+    """
+    Permet à l'utilisateur de rechercher une salle disponible en fonction de certains critères. 
+    Si une salle est trouvée, elle est réservée pour la date spécifiée 
+    et une page de reçu est affichée avec les détails de la réservation. 
+    Si aucune salle n'est trouvée, une page est affichée avec les salles disponibles qui correspondent 
+    le plus aux critères spécifiés.
+    """
     if request.method == 'POST':
         form = SearchForm(request.POST)
         if form.is_valid():
+            #Récupérer les données du formulaire
             nombre_invite = form.cleaned_data['invite_nombre']
             date_f = form.cleaned_data['date']
-            date_string = date_f.strftime("%Y-%m-%d")
+            """
+            convertit la date récupérée depuis le formulaire (date_f) au format "année-mois-jour" (YYYY-MM-DD), 
+            permet de stocker la date dans la session pour le futur usage dans la fonction form_submit.
+            """
+            date_string = date_f.strftime("%Y-%m-%d") 
             request.session['date_f'] = date_string
             request.session.save()
+            
             machine_a_cafe = form.cleaned_data['la_machine_a_cafe']
             tableau_blanc = form.cleaned_data['le_tableau_blanc']
             wifi = form.cleaned_data['la_wifi']
             projecteur = form.cleaned_data['le_projecteur']
             
 
-            
+            #filtrer les salles disponibles en fonction des critères de l'utilisateur
 
             infos_salle_results = InfosSalle.objects.filter(
                 nombre_invite=nombre_invite,
@@ -70,12 +92,15 @@ def search_view(request):
             
             if infos_salle_results:
                 for room in infos_salle_results:
+                    # Recherche des salles réservées pour la date sélectionnée
                     salle_reserve=SalleReserve.objects.filter(nom=room.nom, date_evt=date_f)
             
                     if not salle_reserve:
+                        # Si aucune salle n'est réservée pour cette date, on enregistre la réservation
                             reserved_room = SalleReserve(nom=room.nom, date_evt=date_f)
                             reserved_room.save()
                     
+                            # Retourne la vue de confirmation de réservation
                             return render(request, 'receipt.html', {
                                 'nombre_invite': nombre_invite,
                                 'date': date_f,
@@ -85,10 +110,17 @@ def search_view(request):
                                 'projecteur': projecteur,
                                 'nom_t': room.nom,
                                 'reserved_room':reserved_room,
-                            })
+                            })                        
+                            
+                    # Si la salle est déjà réservée pour cette date, on affiche les salles disponibles
                     elif salle_reserve :
-                        
+                        """
+                        Récupération des noms des salles réservées pour la date sélectionnée pour futur usage 
+                        dans le stockage de données dans la table SalleReserve
+                        """
                         reserved_rooms = SalleReserve.objects.filter(date_evt=date_f).values('nom')
+                        
+                        # Récupération des informations des salles disponibles (en excluant les salles réservées pour cette date)
                         available_rooms_infos = InfosSalle.objects.filter(en_construction=False).exclude(nom__in=Subquery(reserved_rooms))
 
 
@@ -119,11 +151,19 @@ def search_view(request):
               
 
 def form_submit(request):
+    #récupère les données de la session pour obtenir la date sélectionnée par l'utilisateur
     date_string = request.session.get('date_f')
+    #récupère l'ID de la salle sélectionnée dans le tableau des salles disponibles
     room_id = request.POST.get('room_id')
+    """
+    récupère l'objet InfosSalle correspondant à cet ID, crée un nouvel objet SalleReserve
+    avec les informations de cette salle et de la date, et enregistre cet objet en base de données.
+    """
     room = InfosSalle.objects.get(id=room_id)
     reserved_room = SalleReserve(nom=room.nom, date_evt=date_string)
     reserved_room.save()
+    
+    #affiche le reçu avec les informations de la réservation.
     return render(request, 'receipt.html', {
             'nombre_invite': room.nombre_invite,
             'date': date_string,
